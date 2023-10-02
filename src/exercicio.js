e35db16aad407b865e2dc0af1cd326308b1b7b21
Allan Cardoso Araujo <allancardoso.dev@gmail.com>
2023-10-02 00:17:05 -0300

    Adicionando código TypeScript

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(X, Y) {
+    return X * Y;
+}
+console.log(multiplicacao(50, 100));
+function olaNome(nome) {
+    return `Olá, ${nome}!`;
+}
+console.log(olaNome('Allan'));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao(X: number, Y: number) {
+    return X * Y
+}
+console.log(multiplicacao(50, 100));
+
+
+function olaNome(nome:string) {
+
+    return `Olá, ${nome}!`;
+}
+console.log(olaNome('Allan'));
